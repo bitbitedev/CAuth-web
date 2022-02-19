bf9bbbfb9e1e57e6630d299e459c3c3287ba1d09
Ben <benmihm@netcode.dev>
2022-02-19 12:49:09 +0100

    Added CAuth Logo

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 		<?php
 			}
 		?>
-		<link rel="shortcut icon" href="/images/logo64x64.png" type="image/png">
+		<link rel="shortcut icon" href="/assets/image/CAuth-logo.svg" type="image/svg">
         <link rel="manifest" href="/manifest.json">
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700&display=swap" crossorigin>
@@ -48,6 +48,7 @@
 		<header>
 			<div class="content">
 				<a href="/">
+					<img src="/assets/image/CAuth-logo.svg" alt="CAuth Logo">
 					<span>C-Auth</span>
 				</a>
 				<menu>
