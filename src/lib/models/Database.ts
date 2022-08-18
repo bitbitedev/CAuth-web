5f6d5a3955cf51f60614a7a83e413b4ae979b1eb
Benedict Mihm <benmihm@netcode.dev>
2022-08-18 15:38:23 +0200

    Moved api to API Project

--- src/lib/models/Database.ts
+++
@@ -1,6 +0,0 @@
-import { firebaseConfig } from '$lib/firebaseConfig';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
