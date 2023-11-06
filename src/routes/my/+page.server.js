d533bf158203577d09b519f7c3cfb80d02d2829a
netcodedev <benmondini@netcode.dev>
2023-11-06 18:35:26 +0100

    Added route to view all auth requests

--- src/routes/my/+page.server.js
+++ src/routes/my/+page.server.js
@@ -4,4 +4,10 @@ export const load = async ({ locals }) => {
 	if (!locals.db) {
 		throw redirect(302, '/login');
 	}
+	const recentAuths = await locals.db.query('fn::firstDayOfWeek(time::now()); SELECT * FROM authRequest WHERE createdAt > fn::firstDayOfWeek(time::now()) ORDER BY createdAt DESC');
+
+	return {
+		firstDayOfWeek: new Date(recentAuths[0].result).toLocaleDateString('de-DE'),
+		recentAuths: recentAuths[1].result
+	};
 };
--- src/routes/my/+page.svelte
+++ src/routes/my/+page.svelte
@@ -1,1 +1,18 @@
-Hi
+<script>
+    export let data;
+</script>
+
+<div class="card bg-base-100">
+    <div class="card-body">
+        <div class="card-title">
+            Profile
+        </div>
+        <div class="stats shadow">
+            <div class="stat">
+                <div class="stat-title">Auth Requests this week</div>
+                <div class="stat-value">{data.recentAuths.length}</div>
+                <div class="stat-desc">since {data.firstDayOfWeek}</div>
+            </div>
+        </div>
+    </div>
+</div>
