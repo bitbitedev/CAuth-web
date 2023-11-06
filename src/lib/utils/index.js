19f21bc17150abf4a4643e620587f26037f08bcf
netcodedev <benmondini@netcode.dev>
2023-11-06 18:33:47 +0100

    Changed validatePlatformUrl to accept localhost

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -41,6 +41,6 @@ export function validatePlatformUrl(url) {
 	if (!url) {
 		return false;
 	}
-	const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/i;
+	const regex = /^https?:\/\/(?:(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}|localhost)\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/i;
 	return regex.test(url);
 }
