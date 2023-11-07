0523ac15eb675262619ccfd392b0c14194abbb5a
netcodedev <benmondini@netcode.dev>
2023-11-07 15:51:20 +0100

    Better error handling

--- src/routes/api/v1/session/verify/+server.js
+++ src/routes/api/v1/session/verify/+server.js
@@ -1,12 +1,17 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
 
 export async function POST({ request }) {
 	let { session } = await request.json();
 	const authToken = request.headers.get('authorization').replace('Bearer ', '');
 	const _db = db();
-	await _db.authenticate(authToken);
+	try {
+		await _db.authenticate(authToken);
+	} catch (e) {
+		throw error(401, 'Unauthorized');
+	};
 	const [authReq] = await _db.select(`authRequest:${session}`);
-	if (!authReq) return json({ status: 404, error: 'Session not found' });
+	if (!authReq)
+		throw error(404, 'Session not found');
 	return json({ status: 200, session: authReq });
 }
