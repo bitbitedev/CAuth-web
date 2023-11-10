dfd86497bf11cd37333d2d54d2035d4d4e365bae
netcodedev <benmondini@netcode.dev>
2023-11-10 10:52:05 +0100

    Fixed fetching platform not working

--- src/routes/(authentication)/login/+page.server.js
+++ src/routes/(authentication)/login/+page.server.js
@@ -102,17 +102,13 @@ const getPlatformByName = async (name) => {
 	if (name !== null && name.length === 0)
 		throw error(400, { message: 'Invalid request. Specify referrer or remove the ref parameter' });
 	else if (name) {
-		const platform = await rootDB.query('SELECT * FROM platform WHERE name = $name', {
+		const [[ platform ]] = await rootDB.query('SELECT * FROM platform WHERE name = $name', {
 			name: name
 		});
-		if (platform[0].status !== 'OK' || !Array.isArray(platform[0].result)) {
-			throw error(500, { message: 'Error reading data' });
-		}
-		const platformData = platform[0].result;
-		if (platformData.length === 0) {
+		if (!platform) {
 			throw error(500, { message: 'Platform not found' });
 		}
 
-		return platformData[0];
+		return platform;
 	} else return undefined;
 };
