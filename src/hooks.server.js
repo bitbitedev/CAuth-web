28359427e1e3e0a99774630ddd2deead3b30afdf
netcodedev <benmondini@netcode.dev>
2023-11-14 18:59:04 +0100

    Make sure to always close the db connection

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,22 +1,32 @@
 import { db } from '$lib/server/db';
 
 export const handle = async ({ event, resolve }) => {
-	let token = event.cookies.get('token');
-	const _db = db();
-	if (token) {
-		try {
-			if (!(await _db.authenticate(token))) {
+	let response;
+	try {
+		let token = event.cookies.get('token');
+		const _db = db();
+		if (token) {
+			try {
+				if (!(await _db.authenticate(token))) {
+					event.cookies.delete('token');
+				} else {
+					event.locals.db = _db;
+					event.locals.loggedIn = true;
+					event.locals.settings = (await _db.select('userSettings'))[0];
+				}
+			} catch (e) {
 				event.cookies.delete('token');
-			} else {
-				event.locals.db = _db;
-				event.locals.loggedIn = true;
-				event.locals.settings = (await _db.select('userSettings'))[0];
 			}
-		} catch (e) {
-			event.cookies.delete('token');
 		}
+		response = await resolve(event);
+	} catch(err) {
+		response = new Response("Something went wrong.", {
+			status: 500,
+			statusText: 'Internal Server Error'
+		})
+	} finally {
+		_db.close();
 	}
-	const response = await resolve(event);
-	_db.close();
+	
 	return response;
 };
