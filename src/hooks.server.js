4d7c280b73bdd2eee4a5aa2399bd7b0ad7c15ea9
netcodedev <benmondini@netcode.dev>
2023-11-15 12:08:50 +0100

    Fixed undefined variable

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,22 +2,22 @@ import { db } from '$lib/server/db';
 
 export const handle = async ({ event, resolve }) => {
 	let response;
-	try {
-		let token = event.cookies.get('token');
-		const _db = db();
-		if (token) {
-			try {
-				if (!(await _db.authenticate(token))) {
-					event.cookies.delete('token');
-				} else {
-					event.locals.db = _db;
-					event.locals.loggedIn = true;
-					event.locals.settings = (await _db.select('userSettings'))[0];
-				}
-			} catch (e) {
+	let token = event.cookies.get('token');
+	const _db = db();
+	if (token) {
+		try {
+			if (!(await _db.authenticate(token))) {
 				event.cookies.delete('token');
+			} else {
+				event.locals.db = _db;
+				event.locals.loggedIn = true;
+				event.locals.settings = (await _db.select('userSettings'))[0];
 			}
+		} catch (e) {
+			event.cookies.delete('token');
 		}
+	}
+	try {
 		response = await resolve(event);
 	} catch(err) {
 		response = new Response("Something went wrong.", {
