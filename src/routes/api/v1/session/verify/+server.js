8dec744132777a20d65802ca5197359af011c16b
netcodedev <benmondini@netcode.dev>
2023-11-17 19:04:06 +0100

    Changed session route to validate a session instead of authRequest

--- src/routes/api/v1/session/verify/+server.js
+++ src/routes/api/v1/session/verify/+server.js
@@ -3,15 +3,19 @@ import { db } from '$lib/server/db';
 
 export async function POST({ request }) {
 	let { session } = await request.json();
-	const authToken = request.headers.get('authorization').replace('Bearer ', '');
-	const _db = db();
+	const authToken = request.headers.get('authorization')?.replace('Bearer ', '');
+	if(!authToken)
+		throw error(401, 'Unauthorized');
+	const _db = await db();
 	try {
 		await _db.authenticate(authToken);
 	} catch (e) {
 		throw error(401, 'Unauthorized');
 	};
-	const [authReq] = await _db.select(`authRequest:${session}`);
-	if (!authReq)
+	const [ sess ] = await _db.select(`session:${session}`);
+	if (!sess)
 		throw error(404, 'Session not found');
-	return json({ status: 200, session: authReq });
+	if(!sess.valid)
+		throw error(403, 'Session expired');
+	return json({ status: 200, session: sess });
 }
