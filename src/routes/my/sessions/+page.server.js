1b299a5f13f0cc2366e8f1091c05ab88ed6c6a0a
netcodedev <benmondini@netcode.dev>
2023-11-17 15:17:02 +0100

    Added ability to invalidate sessions

--- src/routes/my/sessions/+page.server.js
+++ src/routes/my/sessions/+page.server.js
@@ -1,4 +1,22 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ locals }) {
-	const [ sessions ] = await locals.db.query('SELECT * FROM (SELECT * FROM session) WHERE valid = true ORDER BY createdAt DESC FETCH platform');
-	return { sessions };
+	const [ activeSessions ] = await locals.db.query('SELECT * FROM (SELECT * FROM session) WHERE valid = true ORDER BY createdAt DESC FETCH platform');
+	const [ inactiveSessions ] = await locals.db.query('SELECT * FROM (SELECT * FROM session) WHERE valid = false ORDER BY createdAt DESC FETCH platform');
+	console.log(inactiveSessions);
+	return { activeSessions, inactiveSessions };
+}
+
+const end = async ({ locals, request }) => {
+	if(!locals.loggedIn) throw error(401, 'Unauthorized');
+	const { session } = Object.fromEntries(await request.formData());
+	if(!session)
+		throw error(400, 'Invalid session');
+	await locals.db.merge(`session:${session}`, { 
+		invalidated: true
+	});
+}
+
+export const actions = {
+	end
 }
--- src/routes/my/sessions/+page.svelte
+++ src/routes/my/sessions/+page.svelte
@@ -6,8 +6,7 @@
         month: 'numeric',
         day: 'numeric',
         hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
+        minute: '2-digit'
     };
 </script>
 
@@ -15,19 +14,49 @@
 	<div class="card-body">
 		<div class="card-title">Active Sessions</div>
         <p>Here you can find all active sessions. You can manually disable sessions before they expire to revoke access.</p>
-        {#if data.sessions.length == 0}
+        {#if data.activeSessions?.length == 0}
             No active sessions.
         {/if}
-        <div class="flex flex-wrap">
-            {#each data.sessions as session}
+        <div class="flex flex-wrap gap-2">
+            {#each data.activeSessions as session}
                 <div class="card bg-base-200">
                     <div class="card-body">
                         <div class="card-title">{session.platform.name}</div>
                         <span>Logged in: {new Date(session.createdAt).toLocaleDateString(data.lang, dateSettings)}</span>
                         <span>Expires at: {new Date(session.expiresAt).toLocaleDateString(data.lang, dateSettings)}</span>
+                        <div class="card-actions justify-end mt-2">
+                            <form action="?/end" method="POST">
+                                <button class="btn btn-primary" name="session" value={session.id.split(':')[1]}>End Session</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             {/each}
         </div>
     </div>
 </div>
+
+{#if data.inactiveSessions?.length != 0}
+    <div class="card bg-base-100 shadow-2xl mt-4">
+        <div class="card-body">
+            <div class="card-title">Past Sessions</div>
+            <p>Here you can find all expired sessions.</p>
+            <div class="flex flex-wrap gap-2">
+                {#each data.inactiveSessions as session}
+                    <div class="card card-compact bg-base-200">
+                        <div class="card-body">
+                            <div class="card-title">{session.platform.name}</div>
+                            <span>Logged in: {new Date(session.createdAt).toLocaleDateString(data.lang, dateSettings)}</span>
+                            <span>Expired at: {new Date(session.expiresAt).toLocaleDateString(data.lang, dateSettings)}</span>
+                            {#if session.invalidated}
+                                <span class="error">Invalidated</span>
+                            {:else}
+                                <span class="error">Expired</span>
+                            {/if}
+                        </div>
+                    </div>
+                {/each}
+            </div>
+        </div>
+    </div>
+{/if}
