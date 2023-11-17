c5fe228df5a53be111ccca578bdc45cd81d39c83
netcodedev <benmondini@netcode.dev>
2023-11-17 19:05:58 +0100

    Added api route to get userdata from session

---
+++ src/routes/api/v1/session/user/+server.js
@@ -0,0 +1,24 @@
+import { error, json } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+
+export async function POST({ request }) {
+	let { session } = await request.json();
+	const authToken = request.headers.get('authorization')?.replace('Bearer ', '');
+	if(!authToken)
+		throw error(401, 'Unauthorized');
+	const _db = await db();
+	try {
+		await _db.authenticate(authToken);
+	} catch (e) {
+		throw error(401, 'Unauthorized');
+	};
+	const [ sess ] = await _db.select(`session:${session}`);
+	if (!sess)
+		throw error(404, 'Session not found');
+	if(!sess.valid)
+		throw error(403, 'Session expired');
+    const [[ user ]] = await _db.query('SELECT * FROM type::thing("session",$sess)<-sessions<-user', { 
+        sess: session
+    });
+	return json({ status: 200, user });
+}
