6033bdff50d8977c061ec115c438f8c3a8fe3be7
netcodedev <benmondini@netcode.dev>
2023-11-20 18:26:35 +0100

    Added session invalidation

--- src/routes/(authentication)/logout/+page.server.js
+++ src/routes/(authentication)/logout/+page.server.js
@@ -1,6 +1,16 @@
 import { redirect } from '@sveltejs/kit';
+import { rootDB } from '$lib/server/db';
 
-export async function load({ cookies }) {
-	cookies.delete('token');
-	throw redirect(302, '/');
+export async function load({ cookies, url }) {
+	const sessionId = url.searchParams.get('session');
+	if(!sessionId){
+		cookies.delete('token');
+		throw redirect(303, '/');
+	} else {
+		const [[session]] = await rootDB.query(`SELECT * FROM session:${sessionId} FETCH platform`);
+		if(session){
+			rootDB.merge(session.id, { invalidated: true });
+			throw redirect(303, session.platform.url);
+		}
+	}
 }
