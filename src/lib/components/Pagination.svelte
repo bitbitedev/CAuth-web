ee6a6712b3f36a555319e803303e3368d9a1b203
netcodedev <benmondini@netcode.dev>
2023-11-22 15:37:22 +0100

    Fixed button-group (#579)

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -7,16 +7,16 @@
 </script>
 
 {#if pageCount > 1}
-	<div class="btn-group">
+	<div class="join">
 		<a
 			href={`${url}?${param}=${currentIndex - 1}`}
-			class="btn"
+			class="join-item btn"
 			class:btn-disabled={currentIndex <= 1}>
 			Previous
 		</a>
 		{#if pageCount <= 10}
 			{#each Array(pageCount) as _, i}
-				<a href={`${url}?${param}=${i + 1}`} class="btn" class:btn-active={i + 1 == currentIndex}>
+				<a href={`${url}?${param}=${i + 1}`} class="join-item btn" class:btn-active={i + 1 == currentIndex}>
 					{i + 1}
 				</a>
 			{/each}
@@ -24,23 +24,23 @@
 			<a href={`${url}?${param}=1`} class="btn" class:btn-active={currentIndex == 1}>1</a>
 			{#if currentIndex < 5}
 				{#each Array(Math.min(pageCount, 5) - 1) as _, i}
-					<a href={`${url}?${param}=${i + 2}`} class="btn" class:btn-active={i + 2 == currentIndex}>
+					<a href={`${url}?${param}=${i + 2}`} class="join-item btn" class:btn-active={i + 2 == currentIndex}>
 						{i + 2}
 					</a>
 				{/each}
-				<button class="btn-disabled btn">...</button>
+				<button class="join-item btn-disabled btn">...</button>
 			{:else if currentIndex > pageCount - 4}
-				<button class="btn-disabled btn">...</button>
+				<button class="join-item btn-disabled btn">...</button>
 				{#each Array(4) as _, i}
 					<a
 						href={`${url}?${param}=${pageCount - 4 + i}`}
-						class="btn"
+						class="join-item btn"
 						class:btn-active={pageCount - 4 + i == currentIndex}>
 						{pageCount - 4 + i}
 					</a>
 				{/each}
 			{:else}
-				<button class="btn-disabled btn">...</button>
+				<button class="join-item btn-disabled btn">...</button>
 				{#each Array(5) as _, i}
 					<a
 						href={`${url}?${param}=${currentIndex - 2 + i}`}
@@ -49,18 +49,18 @@
 						{currentIndex - 2 + i}
 					</a>
 				{/each}
-				<button class="btn-disabled btn">...</button>
+				<button class="join-item btn-disabled btn">...</button>
 			{/if}
 			<a
 				href={`${url}?${param}=${pageCount}`}
-				class="btn"
+				class="join-item btn"
 				class:btn-active={pageCount == currentIndex}>
 				{pageCount}
 			</a>
 		{/if}
 		<a
 			href={`${url}?${param}=${currentIndex + 1}`}
-			class="btn"
+			class="join-item btn"
 			class:btn-disabled={currentIndex >= pageCount}>
 			Next
 		</a>
