db295c2a6b90f762ce7f8f8283e5dc30daf72a70
netcodedev <benmondini@netcode.dev>
2023-12-04 12:11:25 +0100

    Added locale settings

--- src/routes/my/settings/+page.svelte
+++ src/routes/my/settings/+page.svelte
@@ -1,5 +1,36 @@
+<script>
+
+	export let data;
+
+	let lang = data.lang;
+
+	const changeSetting = (e) => {
+		const option = e.target.name;
+		const value = e.target.value;
+		const formData = new FormData();
+		formData.append(option, value);
+		fetch('/my/settings/change', {
+			method: 'POST',
+			body: formData
+		});
+	}
+</script>
+
 <h1>Settings</h1>
 <div class="divider m-0"></div>
-<div class="grid grid-cols-2 gap-4 p-4">
+<div class="p-4">
 	Here you can find all settings related to your account.
+	<div class="grid grid-cols-2 gap-4 p-4">
+		<div>Language</div>
+		<select
+			name="lang"
+			id="lang"
+			class="select"
+			on:change={changeSetting}
+			bind:value={lang}>
+			<option value="en-uk">English (UK)</option>
+			<option value="en-us">English (US)</option>
+			<option value="de">German</option>
+		</select>
+	</div>
 </div>
